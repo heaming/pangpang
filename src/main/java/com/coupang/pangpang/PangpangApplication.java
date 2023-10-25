dfe41892984781ad2889454d44825482b5ed4994
조혜미 <hey._.mi@kyowon.co.kr>
2023-10-25 18:16:35 +0900

    Initial commit

---
+++ src/main/java/com/coupang/pangpang/PangpangApplication.java
@@ -0,0 +1,13 @@
+package com.coupang.pangpang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  PangpangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangpangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coupang/pangpang/PangpangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupang.pangpang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangpangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
